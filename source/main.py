0678fbe00d186368f167feaaee267d2869c0d943
Actam55 <leha20@student.aau.dk>
2023-04-12 14:54:06 +0200

    added text to methodology about impl and changed endpoint to our common endpoint in main.py

--- source/main.py
+++ source/main.py
@@ -1,7 +1,8 @@
 from lib import *
 
 # The endpoint of the repository
-endpoint = "http://localhost:7200/repositories/Pokemans-rdf-DB"
+# endpoint = "http://localhost:7200/repositories/Pokemans-rdf-DB"
+endpoint = "http://localhost:7200/repositories/pokemon-repository"
 
 # a SPARQL query to get all the triples in the repository
 query = """
@@ -14,6 +15,6 @@ dataSet = SelectQuery(endpoint, query)
 
 # Create a void description, using a given title, a short description and the data from the query
 voidInsertQuery = VoidCreator("PokemonDB", "A base void description", dataSet)
-
+print(voidInsertQuery)
 # Insert the void description into the repository
-InsertDataQuery(endpoint, voidInsertQuery)
+# InsertDataQuery(endpoint, voidInsertQuery)
