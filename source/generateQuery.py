b396384028da51053206d46c3d99c1a0683c3401
Actam55 <leha20@student.aau.dk>
2023-04-27 09:51:13 +0200

    added the perfect function to get queries

--- source/generateQuery.py
+++ source/generateQuery.py
@@ -1,31 +1,34 @@
 path = "database/two-million.nt"
 
-
-def retrieve_triples(file_path, num_triples):
+# Takes a path to the file and the number of triples to retrieve
+# Retrieve the first n triples from a file and return them as a string
+def retrieve_triples(file_path, num_triples, start = 0):
     with open(file_path, "r") as f:
         lines = f.readlines()
-    triples = []
-    for i in range(len(lines)):
-        if len(triples) >= num_triples:
-            break
+    triples = ""
+    i = start
+    while i <= num_triples and i < len(lines):
         line = lines[i].strip()
-        triples.append(line)
+        triples += line
+        i += 1
     return triples
 
 
-# The amount of arrays - Where for each additional array, the number of triples is doubled - 2^n
-def array_of_triples(file_path, num_arrays):
-    arrayOfArrays = []
+# Takes a path to the file and the number of strings to retrieve
+# For each string, the number of triples retrieved is 2^n - 2, 4, 8... 2^n
+def array_of_triples(file_path, num_strings):
+    array_of_strings = []
     i = 1
-    while i <= num_arrays:
-        arrayOfArrays.append(retrieve_triples(file_path, 2**i))
+    while i <= num_strings:
+        array_of_strings.append(retrieve_triples(file_path, 2**i, 2**(i-1)))
         i += 1
-    return arrayOfArrays
+    return array_of_strings
 
-
-# start from 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 triples- 2^n
-arr = array_of_triples(path, 5)
-
-for i in range(len(arr)):
-    print("Array " + str(i + 1) + " size: " + str(len(arr[i])))
-    print(arr[i])
+# Takes a path to the file and the number of strings to retrieve
+# Creates an array of strings, each string is a query to insert the triples where the number of triples increases exponentially
+def generate_query(file_path, num_strings):
+    array_of_strings = array_of_triples(file_path, num_strings)
+    queries = []
+    for i in range(len(array_of_strings)):
+        queries.append("INSERT DATA {" + array_of_strings[i] + "}")
+    return queries
